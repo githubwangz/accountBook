526b1fe635ef1562fe4a89acdc63966a1a9d7715
_wangz <956185936@qq.com>
2022-06-08 21:05:27 +0800

    修改注释

--- src/main/java/com/accountbook/controller/TestController.java
+++ src/main/java/com/accountbook/controller/TestController.java
@@ -9,7 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("test")
 public class TestController {
 
-    //测试接口
+    /**
+     * 测试使用接口
+     *
+     * @return 随意返回值
+     */
     @GetMapping("/test")
     public String test() {
         return "hello world";
