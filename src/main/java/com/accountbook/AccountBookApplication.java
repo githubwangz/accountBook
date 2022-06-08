bc84c8fa2c48721b22e1a0630a38ddc279210916
githubwangz <78724554+githubwangz@users.noreply.github.com>
2022-06-08 19:02:26 +0800

    修改测试

--- src/main/java/com/accountbook/AccountBookApplication.java
+++ src/main/java/com/accountbook/AccountBookApplication.java
@@ -3,6 +3,7 @@ package com.accountbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//启动项
 @SpringBootApplication
 public class AccountBookApplication {
 
