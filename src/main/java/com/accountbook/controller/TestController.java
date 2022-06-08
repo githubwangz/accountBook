1deb464f171b173befc579301fdf3b49f41dbe78
_wangz <956185936@qq.com>
2022-06-08 20:44:11 +0800

    测试controller

---
+++ src/main/java/com/accountbook/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.accountbook.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "hello world";
+    }
+}
