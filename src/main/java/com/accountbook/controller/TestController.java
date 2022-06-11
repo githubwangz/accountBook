e24a67094165206db6522bb5d179885b341ee755
_wangz <956185936@qq.com>
2022-06-11 16:05:39 +0800

    220611 整合slf4j和log4j2

--- src/main/java/com/accountbook/controller/TestController.java
+++ src/main/java/com/accountbook/controller/TestController.java
@@ -8,14 +8,17 @@ import com.accountbook.utils.MD5Util;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
+
 @RestController
 @RequestMapping("test")
+@Slf4j
 public class TestController {
 
     @Autowired
@@ -29,20 +32,23 @@ public class TestController {
     @GetMapping("/test")
     public Result<String> test() {
         String a = MD5Util.getStringMD5("123456");
+        if (log.isErrorEnabled()){
+            log.error("qingqiuchenggong");
+        }
         return Result.error(ResultMsgEnum.FAIL.getCode(), ResultMsgEnum.FAIL.getMessage());
     }
 
     @PostMapping("/testMybatis")
     public List<Test> testMybatis(@RequestBody Test testEntity) {
         QueryWrapper<Test> queryWrapper = new QueryWrapper<>();
-        Map<String, Object> testMap = JSON.parseObject(JSON.toJSONString(testEntity), new TypeReference<Map<String, Object>>() {
+        Map<String, Object> testMap = JSON.parseObject(JSON.toJSONString(testEntity), new TypeReference<>() {
         });
         testMap.forEach(
                 (k, v) -> {
                     queryWrapper.eq(k, v);
                 }
         );
-        List<Test> result = testService.list(queryWrapper);
-        return result;
+        List<Test> listResult = testService.list(queryWrapper);
+        return listResult;
     }
 }
